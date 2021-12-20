5c5cc693c7ec842de8b7696c99db6d5c9048279e
SH1RL0CK <sh1rl0ck.h0lm1s@gmail.com>
2021-12-20 15:07:38 +0100

    implement basic smtp client

--- thunder_mail_client/smtp_client.cpp
+++ thunder_mail_client/smtp_client.cpp
@@ -1,22 +1,34 @@
 #include "smtp_client.h"
 
-SmtpClient::SmtpClient(QObject *parent) : QObject(parent)
-{
-    tcpSocket = new QTcpSocket(this);
-}
+SmtpClient::SmtpClient(QObject *parent)
+    : QObject(parent)
+    , tcpSocket(new QTcpSocket(this))
+    , state(SmtpClientState::NotConnected)
+    , currentMail(nullptr)
+{}
 
 void SmtpClient::connectToServer(QString ipAdress, int port)
 {
     tcpSocket->connectToHost(ipAdress, port);
     QObject::connect(tcpSocket, &QTcpSocket::readyRead, this, &SmtpClient::receiveText);
-    qDebug() << "connected to server";
+    state = SmtpClientState::ConnectedToServer;
+}
+
+void SmtpClient::sendMail(QString sender, QString recipient, QString subject, QString body)
+{
+    currentMail = new Mail;
+    currentMail->sender = "<" + sender + ">";
+    currentMail->recipient = "<" + recipient + ">";
+    currentMail->subject = subject;
+    currentMail->body = body;
+    sendText("MAIL FROM:" + currentMail->sender);
 }
 
 void SmtpClient::receiveText()
 {
     if(tcpSocket->bytesAvailable())
     {
-        QString receivedText = tcpSocket->readLine();
+        QString receivedText = tcpSocket->readAll();
         qDebug() << "received: " << receivedText;
         handleReceivedText(receivedText);
     }
@@ -30,6 +42,50 @@ void SmtpClient::sendText(QString text)
 
 void SmtpClient::handleReceivedText(QString receivedText)
 {
-    qDebug() << "handle: " << receivedText;
+    int responseCode = receivedText.split(" ").at(0).toInt();
+    if(responseCode == 220 && state == SmtpClientState::ConnectedToServer)
+    {
+        QString clientName = QHostInfo::localHostName();
+        sendText("HELO " + clientName);
+    }
+    else if(responseCode == 250)
+    {
+        switch (state)
+        {
+            case SmtpClientState::SendedVerifyRequest:
+                state = SmtpClientState::VerifiedAtServer;
+                break;
+            case SmtpClientState::StartedSendingNewMail:
+                sendText("RCPT TO:" + currentMail->recipient);
+                state = SmtpClientState::SendedMailRecipient;
+                break;
+            case SmtpClientState::SendedMailRecipient:
+                sendText("DATA");
+                state = SmtpClientState::SendedRequestToSendMailContent;
+                break;
+            default:
+                break;
+        }
+    }
+    else if(responseCode == 354 && state == SendedRequestToSendMailContent)
+    {
+        QString mailContent = "";
+        mailContent += "From: " + currentMail->sender + "\n";
+        mailContent += "To: " + currentMail->recipient  + "\n";
+        mailContent += "Subject: " + currentMail->subject  + "\n";
+        mailContent += "Date: " + QDateTime::currentDateTime().toString("ddd, dd MMM yyyy hh:mm:ss");
+        mailContent += currentMail->body + "\n.";
+        sendText(mailContent);
+        state = SmtpClientState::SendedMailContent;
+    }
+    else if (responseCode == 221)
+    {
+        tcpSocket->close();
+        state = SmtpClientState::NotConnected;
+    }
+    else
+    {
+        qDebug() << "unexpected response!";
+    }
 }
 
--- thunder_mail_client/smtp_client.h
+++ thunder_mail_client/smtp_client.h
@@ -1,33 +1,62 @@
-#ifndef SMPTCLIENT_H
-#define SMPTCLIENT_H
+#ifndef SMTPCLIENT_H
+#define SMTPCLIENT_H
 
 #include <QObject>
 #include <QTcpSocket>
+#include <QHostInfo>
+#include <QDateTime>
 
 /**
- * @brief Die verschiedene Stati, in den sich der SMPT Client gerade befinden kann
+ * @brief Die verschiedene Stati, in den sich der SMTP Client gerade befinden kann
  */
 enum SmtpClientState
 {
     ConnectedToServer,
-    Smtp
+    NotConnected,
+    SendedVerifyRequest,
+    VerifiedAtServer,
+    StartedSendingNewMail,
+    SendedMailRecipient,
+    SendedRequestToSendMailContent,
+    SendedMailContent,
+    SendedQuitRequest,
 };
 
 /**
- * @brief Diese Klasse ist für den SMPT Client zuständig
+ * @brief Struktur zum Speichern der Mail, die gerade verschickt wird
+ */
+struct Mail
+{
+    QString sender = "";
+    QString recipient = "";
+    QString subject = "";
+    QString body = "";
+};
+
+/**
+ * @brief Diese Klasse ist für den SMTP Client zuständig
  */
 class SmtpClient : public QObject
 {
     Q_OBJECT
 public:
+
     explicit SmtpClient(QObject *parent = nullptr);
 
     /**
-     * @brief Der SMPT Client verbindet sich mit dem SMPT Server
-     * @param ipAdress  Die IP-Adresse des SMPT Servers
-     * @param port  Der Port auf dem der SMPT Server läuft
+     * @brief Der SMTP Client verbindet sich mit dem SMTP Server
+     * @param ipAdress  Die IP-Adresse des SMTP Servers
+     * @param port  Der Port auf dem der SMTP Server läuft
      */
     void connectToServer(QString ipAdress, int port);
+    /**
+     * @brief Leitet das Vershicken einer Mail an den Server ein
+     * @param sender  Der Absender der Mail
+     * @param recipient  Der Empfänger der Mail
+     * @param subject  Der Betreff der Mail
+     * @param body  Der Inhalt der Mail
+     */
+    void sendMail(QString sender, QString recipient, QString subject, QString body);
 
 private slots:
     /**
@@ -37,9 +66,17 @@ private slots:
 
 private:
     /**
-     * @brief  Der Socket, der mit dem Server verbunden ist
+     * @brief Der Socket, der mit dem Server verbunden ist
      */
     QTcpSocket *tcpSocket;
+    /**
+     * @brief Der Status in dem sich der SMTP Client gerade befindet
+     */
+    SmtpClientState state;
+    /**
+     * @brief Die Mail, die gereade verschickt wird
+     */
+    Mail *currentMail;
 
     /**
      * @brief Schickt einen Text an der Server
@@ -47,10 +84,10 @@ private:
      */
     void sendText(QString text);
     /**
-     * @brief Verarbeitet die Antworten, die der SMPT Client vom Server empfängt
+     * @brief Verarbeitet die Antworten, die der SMTP Client vom Server empfängt
      * @param receivedText  Der empfangene Text
      */
     void handleReceivedText(QString receivedText);
 };
 
-#endif // SMPTCLIENT_H
+#endif // SMTPCLIENT_H

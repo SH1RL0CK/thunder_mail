71e4a3d7b084770ff13e746c5cc3e9c0d8895cd0
sh1rl0ck <sh1rl0ck@users.noreply.github.com>
2022-01-10 22:00:50 +0100

    add pop3 client

---
+++ thunder_mail_client/pop3_client.cpp
@@ -0,0 +1,139 @@
+#include "pop3_client.h"
+
+Pop3Client::Pop3Client(QObject *parent)
+    : QObject(parent)
+    , tcpSocket(new QTcpSocket)
+    , state(Pop3ClientState::Pop3NotConnected)
+    , numberOfAllMails(0)
+    , currentMailIndex(0)
+{
+    QObject::connect(tcpSocket, &QTcpSocket::readyRead, this, &Pop3Client::receiveText);
+}
+
+Pop3ClientState Pop3Client::getState()
+{
+    return state;
+}
+
+QString Pop3Client::getUsername()
+{
+    return username;
+}
+
+QList<Pop3ClientMail> Pop3Client::getReceivedMails()
+{
+    return receivedMails;
+}
+
+
+void Pop3Client::connectToServer(QString ipAdress, unsigned int port, QString _username, QString _password)
+{
+    tcpSocket->connectToHost(ipAdress, port);
+    state = Pop3ClientState::Pop3ConnectedToServer;
+    username = _username;
+    password = _password;
+}
+
+void Pop3Client::getAllMails()
+{
+    sendText("STAT");
+    state = Pop3ClientState::Pop3SendedStatCommand;
+}
+
+void Pop3Client::receiveText()
+{
+    if(tcpSocket->bytesAvailable())
+    {
+        QString receivedText = tcpSocket->readAll();
+        qDebug() << "received: " << receivedText;
+        handleReceivedText(receivedText);
+    }
+}
+
+void Pop3Client::sendText(QString text)
+{
+    if(state != Pop3ClientState::Pop3NotConnected)
+    {
+        tcpSocket->write(text.toLatin1());
+        qDebug() << "send: " << text;
+    }
+}
+
+void Pop3Client::handleReceivedText(QString receivedText)
+{
+    QString responseStatus = receivedText.split(" ").at(0);
+    if(responseStatus == "+OK")
+    {
+        switch(state)
+        {
+            case Pop3ClientState::Pop3ConnectedToServer:
+                sendText("USER " + username);
+                state = Pop3ClientState::Pop3SendedUsername;
+                break;
+            case Pop3ClientState::Pop3SendedUsername:
+                sendText("PASS " + password);
+                state = Pop3ClientState::Pop3SendedPassword;
+                break;
+           case Pop3ClientState::Pop3SendedPassword:
+                state = Pop3ClientState::Pop3VerifiedAtServer;
+                emit verifiedSuccessfully();
+                break;
+           case Pop3ClientState::Pop3SendedStatCommand:
+                numberOfAllMails = receivedText.split(" ").at(1).toInt();
+                receivedMails.clear();
+                currentMailIndex = 0;
+                state = Pop3ClientState::Pop3WaitingForMails;
+                getNextMail();
+                break;
+           case Pop3ClientState::Pop3WaitingForMails:
+                Pop3ClientMail mail = createPop3ClientMail(receivedText.section("\n", 1));
+                qDebug() << mail.sender;
+                qDebug() << mail.body;
+                receivedMails.append(mail);
+                getNextMail();
+                break;
+        }
+    }
+}
+
+void Pop3Client::getNextMail()
+{
+    currentMailIndex++;
+    if(currentMailIndex > numberOfAllMails)
+    {
+        state = Pop3ClientState::Pop3VerifiedAtServer;
+        currentMailIndex = 0;
+        numberOfAllMails = 0;
+        emit receivedAllMails();
+        return;
+    }
+    sendText("RETR " + QString::number(currentMailIndex));
+}
+
+Pop3ClientMail Pop3Client::createPop3ClientMail(QString content)
+{
+    Pop3ClientMail mail;
+    QStringList header = content.section("\n\r\n", 0,0).split("\n");
+    for(int i = 0; i < header.size(); i++)
+    {
+        if(header.at(i).startsWith("From: "))
+        {
+            mail.sender = header.at(i).section(" ", 1);
+        }
+        else if(header.at(i).startsWith("To: "))
+        {
+            mail.recipients = header.at(i).section(" ", 1);
+        }
+        else if(header.at(i).startsWith("Date: "))
+        {
+            mail.dateTime = header.at(i).section(" ", 1);
+        }
+        else if(header.at(i).startsWith("Subject: "))
+        {
+            mail.subject = header.at(i).section(" ", 1);
+        }
+    }
+    mail.body = content.section("\n\r\n", 1);
+    mail.body.chop(2);
+    return mail;
+}
---
+++ thunder_mail_client/pop3_client.h
@@ -0,0 +1,102 @@
+#ifndef POP3CLEINT_H
+#define POP3CLEINT_H
+
+#include <QObject>
+#include <QTcpSocket>
+
+struct Pop3ClientMail
+{
+    QString sender;
+    QString recipients;
+    QString dateTime;
+    QString subject;
+    QString body;
+};
+
+/**
+ * @brief Die verschiedene Stati, in den sich der POP3-Client gerade befinden kann
+ */
+enum Pop3ClientState
+{
+    Pop3NotConnected,
+    Pop3ConnectedToServer,
+    Pop3SendedUsername,
+    Pop3SendedPassword,
+    Pop3VerifiedAtServer,
+    Pop3SendedStatCommand,
+    Pop3WaitingForMails,
+    Pop3SendedQuitRequest,
+    Pop3SendedNoOperationCommand,
+    Pop3SendedResetRequest,
+};
+
+class Pop3Client : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Pop3Client(QObject *parent = nullptr);
+
+    Pop3ClientState getState();
+    QString getUsername();
+    /**
+     * @brief Der POP3-Client verbindet sich mit dem POP3-Server
+     * @param ipAdress Die IP-Adresse des POP3-Servers
+     * @param port Der Port auf dem der POP3-Server läuft
+     */
+    void connectToServer(QString ipAdress, unsigned int port, QString _username, QString _password);
+
+    void getAllMails();
+
+    QList<Pop3ClientMail> getReceivedMails();
+
+signals:
+    /**
+     * @brief Der Client hat sich erfolgreich am Server verifiziert
+     */
+    void verifiedSuccessfully();
+
+    void receivedAllMails();
+
+private slots:
+    /**
+     * @brief Der Client empfängt Text vom Server
+     */
+    void receiveText();
+
+
+private:
+    /**
+     * @brief Der Socket, der mit dem Server verbunden ist
+     */
+    QTcpSocket *tcpSocket;
+    /**
+     * @brief Der Status, in dem sich der POP3-Client gerade befindet
+     */
+    Pop3ClientState state;
+
+    /**
+     * @brief Die Anmeldedaten am POP3-Server
+     */
+    QString username;
+    QString password;
+
+    int numberOfAllMails;
+
+    int currentMailIndex;
+
+    QList<Pop3ClientMail> receivedMails;
+    /**
+     * @brief Schickt einen Text an der Server
+     * @param text Der zur schickende Text
+     */
+    void sendText(QString text);
+    /**
+     * @brief Verarbeitet die Antworten, die der POP3-Client vom Server empfängt
+     * @param receivedText Der empfangene Text
+     */
+    void handleReceivedText(QString receivedText);
+    void getNextMail();
+    Pop3ClientMail createPop3ClientMail(QString content);
+};
+
+#endif // POP3CLEINT_H
